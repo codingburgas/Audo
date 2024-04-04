15773280cd7c5a5ee6a0485b2b1d289416fe07f9
amkolev22 <amkolev22@codingburgas.bg>
2024-04-04 20:45:34 +0300

    Add automatic setup

---
+++ vs2022.py
@@ -0,0 +1,23 @@
+import subprocess
+import platform
+
+class Colors:
+    CHOOSE_PLATFORM = "\u001b[34m"
+    VS2022 = "\u001b[35m"
+    VS2017 = "\u001b[33m"
+    MAKEFILE = "\u001b[32m"
+    OPTION = "\u001b[36m"
+    ERROR = "\u001b[31m"
+
+options = [f"Visual Studio 2022{Colors.VS2017}", f"Visual Studio 2017{Colors.MAKEFILE}", f"Makefile {Colors.OPTION}"]
+
+def choose_platform():
+    return "vs2022"
+
+selected_option = choose_platform()
+
+if platform.system() == "Windows":
+    subprocess.run(f"start vendor/premake/premake5.exe {selected_option}", shell=True)
+elif platform.system() == "Linux":
+    subprocess.run(f"sudo chmod +x ./vendor/premake/premake5", shell=True)
+    subprocess.run(f"./vendor/premake/premake5 {selected_option}", shell=True)
