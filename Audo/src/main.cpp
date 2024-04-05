0b56470aefb9c0f00d28be33b69fe4f833ecec29
AMKolev22 <amkolev22@codingburgas.bg>
2024-04-05 15:44:08 +0300

    Fix camera bounds

--- Audo/src/main.cpp
+++ Audo/src/main.cpp
@@ -15,7 +15,7 @@ int main() {
     InitWindow(Window::width, Window::height, "fortnite");
 
     Camera2D camera = { 0 };
-    camera.zoom = 0.25f;
+    camera.zoom = 0.36f;
 
     int counter = 0;
 
@@ -25,15 +25,54 @@ int main() {
     //SetTargetFPS(144);
     while (!WindowShouldClose()) {
 
-        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
-        {
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             Vector2 delta = GetMouseDelta();
             delta = Vector2Scale(delta, -1.0f / camera.zoom);
-
             camera.target = Vector2Add(camera.target, delta);
 
-            //if (camera.target.x - (Window::width / 2) * camera.zoom < 0) camera.target.x = (Window::width / 2) * camera.zoom;
-            //if (camera.target.x - (Window::width / 2) * camera.zoom > TILE_SIZE * 1024) camera.target.x = (TILE_SIZE * 1024) - camera.target.x - (Window::width / 2) * camera.zoom;
+
+
+            if (camera.target.x - (Window::width / 2) * camera.zoom < 0) camera.target.x = (Window::width / 2) * camera.zoom;
+            if (camera.target.y - (Window::height / 2) * camera.zoom < 0) camera.target.y = (Window::height / 2) * camera.zoom;
+
+
+            std::vector<float> targetXValues = {
+                19253.25f, 19104.75f, 18938.273438f, 18765.167969f, 18591.777344f,
+                18396.193359f, 18189.298828f, 17976.751853f, 17764.203878f, 17469.074219f,
+                17234.947266f, 16931.408203f, 16607.429688f, 16250.011719f, 15873.304688f,
+                15479.590820f, 14998.400391f, 14502.624023f, 13959.341797f, 13323.376953f,
+                12606.097656f, 11823.172852f, 10893.182617f, 9871.197266f, 8621.201172f,
+                7165.808594f, 5383.988770f
+            };
+
+
+            for (int i = 0; i < targetXValues.size(); i++) {
+                float zoomLowerBound = 0.36f - i * 0.01f - 0.001f;
+                float zoomUpperBound = 0.36f - i * 0.01f + 0.001f;
+                if (camera.zoom > zoomLowerBound && camera.zoom < zoomUpperBound && camera.target.x > targetXValues[i]) {
+                    camera.target.x = targetXValues[i];
+                    break;
+                }
+            }
+
+
+            std::vector<float> targetYValues = {
+             19253.25f, 19104.75f, 18938.273438f, 18765.167969f, 18591.777344f,
+             18396.193359f, 18189.298828f, 17976.751853f, 17764.203878f, 17469.074219f,
+             17234.947266f, 16931.408203f, 16607.429688f, 16250.011719f, 15873.304688f,
+             15479.590820f, 14998.400391f, 14502.624023f, 13959.341797f, 13323.376953f,
+             12606.097656f, 11823.172852f, 10893.182617f, 9871.197266f, 8621.201172f,
+             7165.808594f, 5383.988770f
+            };
+
+            for (int i = 0; i < targetYValues.size(); i++) {
+                float zoomLowerBound = 0.36f - i * 0.01f - 0.001f;
+                float zoomUpperBound = 0.36f - i * 0.01f + 0.001f;
+                if (camera.zoom > zoomLowerBound && camera.zoom < zoomUpperBound && camera.target.y > targetYValues[i]) {
+                    camera.target.y = targetYValues[i];
+                    break;
+                }
+            }
         }
         const float zoomIncrement = 0.1;
 
@@ -46,7 +85,7 @@ int main() {
 
 			camera.zoom += wheel * zoomIncrement;
 
-            if (camera.zoom < 0.25) camera.zoom = 0.25;
+            if (camera.zoom < 0.12) camera.zoom = 0.12;
             else if (camera.zoom > 0.75) camera.zoom = 0.75;
 
 			Vector2 after = GetScreenToWorld2D(mouse, camera);
