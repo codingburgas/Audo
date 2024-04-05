ea2663a1be155cae9d60d8986a9c87027e951d34
AMKolev22 <amkolev22@codingburgas.bg>
2024-04-05 17:33:13 +0300

    Refactor camera bounds code

--- Audo/src/main.cpp
+++ Audo/src/main.cpp
@@ -35,28 +35,7 @@ int main() {
             if (camera.target.x - (Window::width / 2) * camera.zoom < 0) camera.target.x = (Window::width / 2) * camera.zoom;
             if (camera.target.y - (Window::height / 2) * camera.zoom < 0) camera.target.y = (Window::height / 2) * camera.zoom;
 
-
-            std::vector<float> targetXValues = {
-                19253.25f, 19104.75f, 18938.273438f, 18765.167969f, 18591.777344f,
-                18396.193359f, 18189.298828f, 17976.751853f, 17764.203878f, 17469.074219f,
-                17234.947266f, 16931.408203f, 16607.429688f, 16250.011719f, 15873.304688f,
-                15479.590820f, 14998.400391f, 14502.624023f, 13959.341797f, 13323.376953f,
-                12606.097656f, 11823.172852f, 10893.182617f, 9871.197266f, 8621.201172f,
-                7165.808594f, 5383.988770f
-            };
-
-
-            for (int i = 0; i < targetXValues.size(); i++) {
-                float zoomLowerBound = 0.36f - i * 0.01f - 0.001f;
-                float zoomUpperBound = 0.36f - i * 0.01f + 0.001f;
-                if (camera.zoom > zoomLowerBound && camera.zoom < zoomUpperBound && camera.target.x > targetXValues[i]) {
-                    camera.target.x = targetXValues[i];
-                    break;
-                }
-            }
-
-
-            std::vector<float> targetYValues = {
+            std::vector<float> targetValues = {
              19253.25f, 19104.75f, 18938.273438f, 18765.167969f, 18591.777344f,
              18396.193359f, 18189.298828f, 17976.751853f, 17764.203878f, 17469.074219f,
              17234.947266f, 16931.408203f, 16607.429688f, 16250.011719f, 15873.304688f,
@@ -65,14 +44,23 @@ int main() {
              7165.808594f, 5383.988770f
             };
 
-            for (int i = 0; i < targetYValues.size(); i++) {
+
+
+            for (unsigned int i = 0; i < targetValues.size(); i++) {
                 float zoomLowerBound = 0.36f - i * 0.01f - 0.001f;
                 float zoomUpperBound = 0.36f - i * 0.01f + 0.001f;
-                if (camera.zoom > zoomLowerBound && camera.zoom < zoomUpperBound && camera.target.y > targetYValues[i]) {
-                    camera.target.y = targetYValues[i];
-                    break;
+                if (camera.zoom > zoomLowerBound && camera.zoom < zoomUpperBound && camera.target.x > targetValues[i]) {
+                    if (camera.target.x > targetValues[i]) {
+                        camera.target.x = targetValues[i];
+                        break;
+                    }
+                    if (camera.target.y > targetValues[i]) {
+                        camera.target.y = targetValues[i];
+                        break;
+                    }
                 }
             }
+
         }
         const float zoomIncrement = 0.1;
 
